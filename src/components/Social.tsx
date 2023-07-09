9dbd6c7e4a3e5bc5d4348967256d43a91277d683
alsongarbuja <garbuja12alson@gmail.com>
2023-07-09 22:51:31 +0545

    update: update link to facebook

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -5,15 +5,15 @@ import { Facebook, Instagram, Linkedin } from 'react-feather'
 const Social = () => {
   return (
     <nav className="flex items-center gap-10">
-        <Link href="https://www.linkedin.com/company/thanka-digital/" target="_blank">
-            <Linkedin />
-        </Link>
-        <Link href="https://www.instagram.com/thanka.digital/" target="_blank">
-            <Instagram />
-        </Link>
-        <Link href="https://www.facebook.com/thankadigital/" target="_blank">
-            <Facebook />
-        </Link>
+      <Link href="https://www.linkedin.com/company/thanka-digital/" target="_blank" rel="noreferrer">
+        <Linkedin />
+      </Link>
+      <Link href="https://www.instagram.com/thanka.digital/" target="_blank" rel="noreferrer">
+        <Instagram />
+      </Link>
+      <Link href="https://www.facebook.com/thankadigital1/" target="_blank" rel="noreferrer">
+        <Facebook />
+      </Link>
     </nav>
   )
 }
