8a8ceae3697b64445d404467eaca680df37e9c2f
alsongarbuja <garbuja12alson@gmail.com>
2023-09-09 13:35:44 +0545

    UI: new hero section and header with responsive design

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,17 +1,23 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 const HeroSection = () => {
   return (
-    <section className="relative flex flex-col items-center justify-center py-16 text-center">
+    <section className="relative flex items-center justify-between">
+      <div>
+        <h1 className="text-primary_red">Thanka <span className="text-primary_blue">Digital</span></h1>
+        <h4 className="mt-2 text-primary_red">CODE CRAFTED FOR QUALITY</h4>
+        <p className="w-full mb-4 md:w-3/4">Leverage our expertise to elevate your business. Our meticulously crafted code and state-of-the-art technologies, backed by our dedicated teams, ensure the delivery of top-tier quality solutions.</p>
+        <button className="p-4 mr-4 text-white rounded-md bg-primary_red">Connect with us</button>
+        <Link href="#" className="font-medium text-primary_blue">View our portfolio</Link>
+      </div>
       <Image 
-        src="/thanka_digital_logo.svg" 
-        alt="thanka digital logo" 
+        src="/hero_section_img.svg" 
+        alt="illustration of young man waving while sitting at the computer" 
         width={250}
         height={650} 
-        className="w-[5em] h-[5em]"
+        className="w-[45rem] hidden md:block"
       />
-      <h1 className="z-20">Let's Take Your <br />Business To Next Level</h1>
-      <h3 className="mt-2 text-primary_red">CODE CRAFTED FOR QUALITY</h3>
     </section>
   )
 }
