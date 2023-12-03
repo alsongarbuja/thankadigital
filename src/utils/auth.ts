c47786a6dfe32b16d6ade98dd842396a95084040
alsongarbuja <magar33alson@gmail.com>
2023-12-03 15:36:30 +0545

    fix: vercel deploy and dynamic server usage error

---
+++ src/utils/auth.ts
@@ -0,0 +1,57 @@
+import { JWT } from "next-auth/jwt";
+import type { AuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import { login } from "@/server/controllers/auth.controller";
+
+export const authOptions: AuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "text" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        if (typeof credentials !== "undefined") {
+          const res = await login(
+            credentials.email,
+            credentials.password
+          );
+          if (typeof res !== "undefined") {
+            return { ...res.user, apiToken: res.token };
+          } else {
+            return null;
+          }
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  session: { strategy: "jwt" },
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async session({ session, token, user }) {
+      const sanitizedToken = Object.keys(token).reduce((p, c) => {
+        // strip unnecessary properties
+        if (c !== "iat" && c !== "exp" && c !== "jti" && c !== "apiToken") {
+          return { ...p, [c]: token[c] };
+        } else {
+          return p;
+        }
+      }, {});
+      return { ...session, user: sanitizedToken, apiToken: token.apiToken };
+    },
+    async jwt({ token, user, account, profile }) {
+      if (typeof user !== "undefined") {
+        // user has just signed in so the user object is populated
+        return user as unknown as JWT;
+      }
+      return token;
+    },
+  },
+};
