414d891180c8bf90751d27b82c47e53b3ac6cdba
alsongarbuja <magar33alson@gmail.com>
2023-12-03 14:53:03 +0545

    permission: add permission in admin side

--- src/app/(admin_side)/_components/AdminSidebar.tsx
+++ src/app/(admin_side)/_components/AdminSidebar.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { Briefcase, FileText, Grid, Users } from "react-feather";
 import { usePathname, useRouter } from "next/navigation";
-import { getFromLocalStorage, removeFromLocalStorage } from "@/helpers/localstorage";
 import { signOut, useSession } from "next-auth/react";
 
 const sidebarLinks = [
@@ -12,24 +11,28 @@ const sidebarLinks = [
     name: "Dashboard",
     path: "/admin/dashboard",
     icon: <Grid />,
+    permissions: ["superadmin", "admin", "content-writer"],
   },
   {
     id: 1,
     name: "Users",
     path: "/admin/users",
     icon: <Users />,
+    permissions: ["superadmin", "admin"],
   },
   {
     id: 2,
     name: "Blogs",
     path: "/admin/blogs",
     icon: <FileText />,
+    permissions: ["superadmin", "admin", "content-writer"],
   },
   {
     id: 3,
     name: "Careers",
     path: "/admin/careers",
     icon: <Briefcase />,
+    permissions: ["superadmin", "admin"],
   },
 ];
 
@@ -37,7 +40,7 @@ const AdminSidebar = () => {
   const pathname = usePathname();
   const router = useRouter();
 
-  const { } = useSession({
+  const { data: session } = useSession({
     required: true,
     onUnauthenticated() {
       router.push("/login");
@@ -51,14 +54,18 @@ const AdminSidebar = () => {
         <span className="inline-block px-4 my-8 text-3xl">Thanka Digital</span>
       </Link>
       <ul>
-        {sidebarLinks.map(link => (
-          <Link href={link.path} key={link.id}>
-            <li className={`flex items-center gap-4 p-4 hover:bg-primary_red/60 active:bg-primary_red ${pathname===link.path&&'bg-primary_red'}`}>
-              {link.icon}
-              {link.name}
-            </li>
-          </Link>
-        ))}
+        {sidebarLinks.map(link => {
+          // @ts-ignore
+          if (!link.permissions.includes(session?.user?.role)) return null;
+          return (
+            <Link href={link.path} key={link.id}>
+              <li className={`flex items-center gap-4 p-4 hover:bg-primary_red/60 active:bg-primary_red ${pathname===link.path&&'bg-primary_red'}`}>
+                {link.icon}
+                {link.name}
+              </li>
+            </Link>
+          )}
+        )}
       </ul>
     </div>
     <button className="py-4 text-primary_red" onClick={() => signOut()}>
