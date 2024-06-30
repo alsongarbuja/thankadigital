94157ed39af27e47b1c2973a20ba45f98fb20388
alsongarbuja <magar33alson@gmail.com>
2024-06-30 16:40:10 +0545

    update: update utils list

---
+++ src/utils/footer.tsx
@@ -0,0 +1,180 @@
+import { Facebook, Linkedin, MessageCircle } from "react-feather";
+
+export const footerConnect = [
+  {
+    location: [
+      {
+        id: 0,
+        location: "Fulbari 11",
+      },
+      {
+        id: 1,
+        location: "Pokhara, Kaski",
+      },
+      {
+        id: 2,
+        location: "Nepal",
+      },
+    ],
+  },
+
+  {
+    connect: [
+      {
+        id: 0,
+        contact: "+977 9825140802",
+      },
+      {
+        id: 1,
+        contact: "+977 9825140801",
+      },
+      {
+        id: 2,
+        contact: "thankadigital@gmail.com",
+      },
+    ],
+  },
+
+  {
+    socialmedia: [
+      {
+        id: 0,
+        name: "linkedin",
+        link: "/",
+      },
+      {
+        id: 1,
+        name: "whatsapp",
+        link: "/",
+      },
+      {
+        id: 2,
+        name: "facebook",
+        link: "/",
+      },
+    ],
+  },
+];
+
+export const footerLinks = [
+  {
+    menuTitle: "Important Links",
+    links: [
+      {
+        name: "About us",
+        link: "#",
+      },
+      {
+        name: "Blogs",
+        link: "#",
+      },
+      {
+        name: "Careers",
+        link: "#",
+      },
+    ],
+  },
+  {
+    menuTitle: "Company",
+    links: [
+      {
+        name: "Youtube",
+        link: "#",
+      },
+      {
+        name: "Discord",
+        link: "#",
+      },
+      {
+        name: "Community",
+        link: "#",
+      },
+    ],
+  },
+];
+
+export const footerContacts = [
+  {
+    id: 0,
+    icon: <Linkedin />,
+    link: "/",
+  },
+  {
+    id: 1,
+    icon: <Facebook />,
+    link: "/",
+  },
+  {
+    id: 2,
+    icon: <MessageCircle />,
+    link: "/",
+  },
+];
+// {
+//   title: 'ADDRESS',
+//   links: [
+//     {
+//       text: 'Remote',
+//       href: null,
+//     },
+//     {
+//       text: 'Nepal 33700',
+//       href: null,
+//     },
+//   ],
+// },
+// {
+//   title: 'CONTACT US',
+//   links: [
+//     {
+//       text: '+977 9825140802',
+//       href: 'tel:9825140802',
+//     },
+//     {
+//       text: '+977 9866011579',
+//       href: 'tel:9866011579',
+//     },
+//     {
+//       text: 'thankadigital@gmail.com',
+//       href: 'mailto:thankadigital@gmail.com',
+//     },
+//   ],
+// },
+// {
+//   title: 'SERVICES',
+//   links: [
+//     {
+//       text: 'Web Development',
+//       href: null,
+//     },
+//     {
+//       text: 'App Development',
+//       href: null,
+//     },
+//     {
+//       text: 'Digital Marketing',
+//       href: null,
+//     },
+//     {
+//       text: 'SEO',
+//       href: null,
+//     },
+//   ],
+// },
+// {
+//   title: 'IMPORTANT LINKS',
+//   links: [
+//     {
+//       text: 'Blogs',
+//       href: '/blogs',
+//     },
+//     {
+//       text: 'Career',
+//       href: '/careers',
+//     },
+//     {
+//       text: 'Admin',
+//       href: '/login',
+//     }
+//   ],
+// }
---
+++ src/utils/services.ts
@@ -0,0 +1,33 @@
+export const servicesDatas = [
+  {
+    title: "Websites",
+    description: "Get your business online fast with stunning looking and performant websites",
+    variant: 'white',
+    colSpan: 2
+  },
+  {
+    title: "Apps",
+    description: "We got you covered not only in web world but also in the mobile application space.",
+    variant: 'black',
+    colSpan: 1
+  },
+  {
+    title: "UI/UX",
+    description: "Looking to spice up your profile with beautiful looking websites, apps and other digital space.",
+    variant: 'white',
+    colSpan: 1
+  },
+  {
+    title: "Marketing",
+    description: "Boost your business with custom and well crafted marketing materials.",
+    variant: 'white',
+    colSpan: 1
+  },
+  {
+    title: "Web Applications",
+    description: "Grow your business with custom curated web application built with modern cutting edge technologies.",
+    variant: 'primary',
+    image: '/images/web-app.jpg',
+    colSpan: 3
+  },
+]
