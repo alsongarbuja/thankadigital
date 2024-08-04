33f5d93595a77b501eee4ffaeeea0eff6b83566f
alsongarbuja <magar33alson@gmail.com>
2024-08-04 17:15:43 +0545

    ui: add blur circle in career page

--- src/app/(main_site)/careers/page.tsx
+++ src/app/(main_site)/careers/page.tsx
@@ -6,15 +6,19 @@ export default async function CareerPage() {
   const careerList: dynamicObject = await getCareers();
 
   return (
-    <CustomWidthWrapper className="py-12">
-      <div className="min-h-[60vh]">
-        <h1>Career opportunities</h1>
-        <p className="pb-12 font-medium text-gray-600 border-b border-gray-200">
-          We are always looking for passionate people to join our team. Feel
-          free to contact us even if you don't see an opening.
-        </p>
-        <CareerList careerList={careerList.careers} />
-      </div>
-    </CustomWidthWrapper>
+    <div className="relative overflow-hidden">
+      <div className="absolute top-0 rounded-lg -right-24 blob blob-rounded"></div>
+      <div className="absolute bottom-0 rounded-lg -left-24 blob blob-rounded"></div>
+      <CustomWidthWrapper className="relative py-12">
+        <div className="min-h-[60vh]">
+          <h1>Career opportunities</h1>
+          <p className="pb-12 font-medium text-gray-600 border-b border-gray-200">
+            We are always looking for passionate people to join our team. Feel
+            free to contact us even if you don't see an opening.
+          </p>
+          <CareerList careerList={careerList.careers} />
+        </div>
+      </CustomWidthWrapper>
+    </div>
   );
 }
