be6d9de1d1e124c8057958a2e19c7840c533270f
alsongarbuja <magar33alson@gmail.com>
2024-08-05 12:11:50 +0545

    update: core members static image listing instead of dynamic

--- src/app/(main_site)/about/_components/CoreMembers.tsx
+++ src/app/(main_site)/about/_components/CoreMembers.tsx
@@ -1,21 +1,7 @@
-"use client";
+import { coreTeams } from "@/utils/team";
 import Image from "next/image";
-import { useEffect, useState } from "react";
 
 const CoreMembers = () => {
-  const [members, setMembers] = useState([]);
-  const [fetching, setFetching] = useState(true);
-
-  const fetchMembers = async () => {
-    const res = await fetch("/api/admin/teams?type=Core");
-    const data = await res.json();
-    setMembers(data.teams.teams);
-  };
-
-  useEffect(() => {
-    fetchMembers().finally(() => setFetching(false));
-  }, []);
-
   return (
     <section className="my-14">
       <div className="mb-8">
@@ -28,36 +14,24 @@ const CoreMembers = () => {
       </div>
 
       <div className="flex flex-wrap items-center gap-x-10 gap-y-24 justify-evenly">
-        {fetching &&
-          Array.from({ length: 5 }).map((_, index) => (
-            <div
-              key={index}
-              className="flex flex-col items-center justify-center gap-4"
-            >
-              <div className="bg-gray-300 rounded-full w-80 h-80 animate-pulse"></div>
-              <div className="h-6 bg-gray-300 w-52 animate-pulse"></div>
-              <div className="h-6 bg-gray-300 w-52 animate-pulse"></div>
-            </div>
-          ))}
-        {!fetching &&
-          members.map((member: TeamModel & { id: string }) => (
-            <div
-              key={member.id}
-              className="flex flex-col items-center justify-center gap-4"
-            >
-              <Image
-                src={member.imageUrl}
-                alt={`photo of ${member.name}`}
-                width={350}
-                height={350}
-                className="object-contain rounded-full w-80 h-80"
-              />
-              <p className="mt-2 text-xl font-bold text-center text-secondary">
-                {member.name}
-              </p>
-              <p className="text-xl font-bold text-center">{member.position}</p>
-            </div>
-          ))}
+        {coreTeams.map((member, index) => (
+          <div
+            key={index}
+            className="flex flex-col items-center justify-center gap-4"
+          >
+            <Image
+              src={`/images/team/${member.image}`}
+              alt={`photo of ${member.name}`}
+              width={350}
+              height={350}
+              className="object-contain rounded-full w-80 h-80"
+            />
+            <p className="mt-2 text-xl font-bold text-center text-secondary">
+              {member.name}
+            </p>
+            <p className="text-xl font-bold text-center">{member.role}</p>
+          </div>
+        ))}
       </div>
     </section>
   );
---
+++ src/utils/team.ts
@@ -0,0 +1,27 @@
+export const coreTeams = [
+  {
+    name: "Alson Garbuja",
+    role: "CEO",
+    image: "alson.jpg",
+  },
+  {
+    name: "Bipin Adhikari",
+    role: "CFO",
+    image: "bipin.jpg",
+  },
+  {
+    name: "Sunil Paudel",
+    role: "COO",
+    image: "sunil.jpg",
+  },
+  {
+    name: "Susant Basnet",
+    role: "CMO",
+    image: "susant.jpg",
+  },
+  {
+    name: "Utsan Gurung",
+    role: "CTO",
+    image: "utsab.jpg",
+  },
+];
