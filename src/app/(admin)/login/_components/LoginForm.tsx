44790fb298bdc11875ff791faea05241a054e17e
Alson Garbuja <magar33alson@gmail.com>
2024-09-23 21:29:26 +0545

    fix: fix login flow callback issue

--- src/app/(admin)/login/_components/LoginForm.tsx
+++ src/app/(admin)/login/_components/LoginForm.tsx
@@ -1,41 +1,48 @@
-"use client"
+"use client";
 
-import { PropsWithChildren } from 'react'
+import { PropsWithChildren } from "react";
 import { useEffect, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
-import { Loader } from 'react-feather';
+import { Loader } from "react-feather";
 import { signIn } from "next-auth/react";
-import { addToLocalStorage } from '@/helpers/localstorage';
+import { addToLocalStorage } from "@/helpers/localstorage";
 
 interface LoginFormProps {
   session: any;
 }
 
-const LoginForm = ({ children, session }: PropsWithChildren<LoginFormProps>) => {
+const LoginForm = ({
+  children,
+  session,
+}: PropsWithChildren<LoginFormProps>) => {
   const search = useSearchParams();
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [error, setError] = useState<string | null>(null);
   const router = useRouter();
 
   const submitLogin = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
-    setIsSubmitting(true)
+    e.preventDefault();
+    setIsSubmitting(true);
 
-    const formData = new FormData(e.currentTarget)
-    const email = formData.get("email")
-    const password = formData.get("password")
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get("email");
+    const password = formData.get("password");
 
     await signIn("credentials", {
       email,
       password,
+      callbackUrl: (search.get("callbackUrl") as string) ?? "/admin/dashboard",
     });
-  }
+  };
 
   useEffect(() => {
     // @ts-ignore
-    if (session?.value !== 'null') {
-      addToLocalStorage("thanka_email", JSON.parse(session?.value)?.user?.email);
-      if(search.get("callbackUrl")) {
+    if (session?.value !== "null") {
+      addToLocalStorage(
+        "thanka_email",
+        JSON.parse(session?.value)?.user?.email
+      );
+      if (search.get("callbackUrl")) {
         router.push(search.get("callbackUrl") as string);
       } else {
         router.push("/admin/dashboard");
@@ -54,13 +61,20 @@ const LoginForm = ({ children, session }: PropsWithChildren<LoginFormProps>) => 
         </div>
       )}
       {children}
-      <button type="submit" className="flex items-center justify-center gap-2 p-2 text-white rounded-md bg-primary_blue">
-        {isSubmitting ? <>
-          Logging in... <Loader className="animate-spin" />
-        </> : "Login"}
+      <button
+        type="submit"
+        className="flex items-center justify-center gap-2 p-2 text-white rounded-md bg-primary_blue"
+      >
+        {isSubmitting ? (
+          <>
+            Logging in... <Loader className="animate-spin" />
+          </>
+        ) : (
+          "Login"
+        )}
       </button>
     </form>
-  )
-}
+  );
+};
 
-export default LoginForm
+export default LoginForm;
